91dcfab6062b398054d32242a0dfab6e151d5414
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-21 18:34:35 +0000

    added handle change

--- todofrontend/src/TodoForm.js
+++ todofrontend/src/TodoForm.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
 
 class TodoForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {inputValue: 'lolhaha'};
+    }
+    
+    handleChange(e) {
+        this.setState{(
+            inputValue: e.target.value;
+            })
+    }
     render() {
         return (
             <div>
-                <input type="text" />
+                <input type="text" 
+                value={this.state.inputValue}
+                onChange={this.handleChange}
+                />
                 <button>Add Todo</button>
             </div>
             )
